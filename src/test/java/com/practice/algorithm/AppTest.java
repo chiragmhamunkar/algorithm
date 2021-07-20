63c84a0e1c4ed5d0abe126f8aac5e3d713fde6d0
Chirag Mhamunkar <chiragmhamunkar23@gmail.com>
2021-07-20 10:39:45 +0530

    Initial setup

---
+++ src/test/java/com/practice/algorithm/AppTest.java
@@ -0,0 +1,15 @@
+package com.practice.algorithm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void assertTest(){
+        Assert.assertTrue(true);
+    }
+}
